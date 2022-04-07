b6a15eaf0227d48645bbe2d20a9bc8b06e4080a1
BigYusuf <obcessedbobos@gmail.com>
2022-04-07 03:59:44 +0100

    fifth heroku commit

--- API/server.js
+++ API/server.js
@@ -28,17 +28,17 @@ app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/allproducts', allproductRouter);// testing purpose remove it later
-
+/*
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build", 'index.html'))
-});
-/*
+});*/
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);*/
+);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
